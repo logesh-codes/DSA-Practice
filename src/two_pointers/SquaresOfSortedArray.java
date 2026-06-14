1aa0f8829698d926577f279556e7e3602f4d5255
logeshp1206 <logeshp1206@gmail.com>
2026-06-14 16:34:40 +0530

    feat(arrays): implement squares of sorted arrays using two pointers

---
+++ src/two_pointers/SquaresOfSortedArray.java
@@ -0,0 +1,36 @@
+package two_pointers;
+
+import java.util.Arrays;
+
+public class SquaresOfSortedArray {
+    public static int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] ans = new int[n];
+
+        int left = 0;
+        int right = n-1;
+        int index = n-1;
+
+        while(left <= right){
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if(leftSquare > rightSquare){
+                ans[index] = leftSquare;
+                left++;
+            }else{
+                ans[index] = rightSquare;
+                right--;
+            }
+            index--;
+        }
+
+        return ans;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {-7,-3,2,3,11};
+        System.out.println(Arrays.toString(sortedSquares(nums)));
+    }
+}
+
