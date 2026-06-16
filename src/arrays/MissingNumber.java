4cb08a0f8abf7f00d57d23740ec6c47d90db11ad
logeshp1206 <logeshp1206@gmail.com>
2026-06-16 19:29:30 +0530

    feat(arrays): added missing number

---
+++ src/arrays/MissingNumber.java
@@ -0,0 +1,24 @@
+package arrays;
+
+public class MissingNumber {
+
+    public static int missingNumber(int[] nums) {
+        int n = nums.length;
+
+        int experted = n*(n+1)/2;
+        int actual = 0;
+
+        for(int num : nums){
+            actual += num;
+        }
+
+        return experted - actual;
+    }
+
+    public static void main(String[] args){
+        int[] nums = {3,0,1};
+
+        System.out.println(missingNumber(nums));
+    }
+
+}
