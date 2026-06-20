4c6d67563ae8546b13112172094f1e7c090db851
logeshp1206 <logeshp1206@gmail.com>
2026-06-20 18:13:25 +0530

    feat(arrays): add build array from permutation solution

---
+++ src/arrays/BuildArrayfromPermutation.java
@@ -0,0 +1,26 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class BuildArrayfromPermutation {
+
+    public static int[] buildArray(int[] nums){
+        int n= nums.length;
+
+        for (int i=0; i<n; i++){
+            nums[i] = nums[i] + ((nums[nums[i]]) % n) * n ;
+        }
+
+        for (int i=0; i<n; i++){
+            nums[i] = nums[i] / n;
+        }
+
+        return nums;
+    }
+
+    public static void main(String[] args){
+        int[] nums =  {0,2,1,5,3,4};
+
+        System.out.println(Arrays.toString(buildArray(nums)));
+    }
+}
