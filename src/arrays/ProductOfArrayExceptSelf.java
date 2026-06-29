6624930ee6680d59a322348fa4167ce661f3f64d
logeshp1206 <logeshp1206@gmail.com>
2026-06-29 22:20:44 +0530

    feat(arrays): add product of array except self solution

---
+++ src/arrays/ProductOfArrayExceptSelf.java
@@ -0,0 +1,33 @@
+package arrays;
+
+import java.util.Arrays;
+
+public class ProductOfArrayExceptSelf {
+
+    public static int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+
+        // store left product
+        int leftProduct = 1;
+        for (int i=0; i<n; i++){
+            result[i] = leftProduct;
+            leftProduct *= nums[i];
+        }
+
+        // multiple with right product
+        int rightProduct = 1;
+        for (int i=n-1; i>=0; i--){
+            result[i] *= rightProduct;
+            rightProduct *= nums[i];
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        System.out.println(Arrays.toString(productExceptSelf(nums)));
+    }
+
+}
