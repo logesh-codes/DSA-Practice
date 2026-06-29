b90d920d47bc767e7bd206bd57af6773e54a0510
logeshp1206 <logeshp1206@gmail.com>
2026-06-29 22:22:59 +0530

    feat(arrays): add maximum subarray solution

---
+++ src/arrays/MaximumSubarray.java
@@ -0,0 +1,27 @@
+package arrays;
+
+public class MaximumSubarray {
+    public static int maxSubArray(int[] nums) {
+        int currentSum = 0;
+        int maxSum = Integer.MIN_VALUE;
+
+        for (int num : nums){
+            currentSum += num;
+
+            maxSum = Math.max(currentSum, maxSum);
+
+            if(currentSum < 0){
+                currentSum = 0;
+            }
+        }
+        return maxSum;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {-2,1,-3,4,-1,2,1,-5,4};
+
+        System.out.println(maxSubArray(nums));
+
+    }
+}
