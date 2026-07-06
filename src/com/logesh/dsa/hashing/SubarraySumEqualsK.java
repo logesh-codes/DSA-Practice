303721e49afd9077f21cb9f2269df131ef2c9148
logeshp1206 <logeshp1206@gmail.com>
2026-07-06 19:16:10 +0530

    feat(hashing): optimize solution for subarray sum equals k using prefix sum and hashmap

--- src/com/logesh/dsa/hashing/SubarraySumEqualsK.java
+++ src/com/logesh/dsa/hashing/SubarraySumEqualsK.java
@@ -1,5 +1,7 @@
 package com.logesh.dsa.hashing;
 
+import java.util.HashMap;
+
 public class SubarraySumEqualsK {
     // Brute Force
     public static int subarraySumbf(int[] nums, int k) {
@@ -19,10 +21,32 @@ public class SubarraySumEqualsK {
         return count;
     }
 
+    public static int subarraySum(int[] nums, int k) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        map.put(0,1);
+
+        int count = 0;
+        int sum = 0;
+
+        for (int num : nums){
+            sum += num;
+
+            if (map.containsKey(sum - k)){
+                count += map.get(sum - k);
+            }
+
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+
+        return count;
+    }
+
     public static void main(String[] args) {
         int[] nums = {1,1,1};
         int k = 2;
 
-        System.out.println(subarraySumbf(nums,k));
+        //System.out.println(subarraySumbf(nums,k));
+        System.out.println(subarraySum(nums, k));
     }
 }
